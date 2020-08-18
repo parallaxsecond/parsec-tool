e5068448f1246787310051fb705c30e86603088c
Joe Ellis <joe.ellis@arm.com>
2020-08-18 09:24:49 +0100

    Add output file options for subcommands
    Signed-off-by: Joe Ellis <joe.ellis@arm.com>

--- src/subcommands/common.rs
+++ src/subcommands/common.rs
@@ -3,6 +3,7 @@
 
 //! Common facilities and options for subcommands.
 
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 /// Options for specifying a provider. Most, but not all subcommands require the user to do this,
@@ -12,4 +13,12 @@ pub struct ProviderOpts {
     /// The provider to list opcodes for.
     #[structopt(short = "p", long = "provider", default_value = "0")]
     pub provider: u8,
+}
+
+/// Options for specifying an output file.
+#[derive(Debug, StructOpt)]
+pub struct OutputFileOpts {
+    /// The output file to write to.
+    #[structopt(parse(from_os_str), short = "o", long = "output")]
+    pub output_file_path: Option<PathBuf>,
 }
