1ddcbf3c35f770107c4838faafa2791425f5d33c
Hugues de Valon <hugues.devalon@arm.com>
2021-01-13 15:44:18 +0000

    Add ListClients and DeleteClient operations
    Signed-off-by: Hugues de Valon <hugues.devalon@arm.com>

---
+++ src/subcommands/delete_client.rs
@@ -0,0 +1,54 @@
+// Copyright 2021 Contributors to the Parsec project.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Delete all data a client has in the service (admin operation).
+
+pub use crate::cli::ParsecToolApp;
+use crate::error::ParsecToolError;
+use crate::subcommands::ParsecToolSubcommand;
+use parsec_client::core::interface::operations::{delete_client, NativeOperation, NativeResult};
+
+use parsec_client::core::interface::requests::ProviderID;
+use parsec_client::core::operation_client::OperationClient;
+use std::convert::TryFrom;
+use structopt::StructOpt;
+
+/// Delete all data a client has in the service (admin operation).
+#[derive(Debug, StructOpt)]
+#[structopt(name = "delete_client")]
+pub struct DeleteClientSubcommand {
+    #[structopt(short = "c", long = "client")]
+    client: String,
+}
+
+impl TryFrom<&DeleteClientSubcommand> for NativeOperation {
+    type Error = ParsecToolError;
+
+    fn try_from(delete_client: &DeleteClientSubcommand) -> Result<Self, Self::Error> {
+        // Trivially converted to a `NativeOperation`.
+        Ok(NativeOperation::DeleteClient(delete_client::Operation {
+            client: delete_client.client.clone(),
+        }))
+    }
+}
+
+impl ParsecToolSubcommand<'_> for DeleteClientSubcommand {
+    fn run(&self, matches: &ParsecToolApp) -> Result<(), ParsecToolError> {
+        let client = OperationClient::new();
+        let native_result = client.process_operation(
+            NativeOperation::try_from(self)?,
+            ProviderID::Core,
+            &matches.authentication_data()?,
+        )?;
+
+        match native_result {
+            NativeResult::DeleteClient(_) => (),
+            _ => {
+                return Err(ParsecToolError::UnexpectedNativeResult(native_result));
+            }
+        };
+
+        success!("Client \"{}\" deleted.", self.client);
+        Ok(())
+    }
+}
