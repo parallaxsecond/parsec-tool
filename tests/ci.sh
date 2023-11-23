84b258ed0dd100f7c145e0b8d30680c7c9f6944b
Tomás González <tomasagustin.gonzalezorlando@arm.com>
2023-11-23 13:47:10 +0000

    ci/nightly: Add a dependency mismatcher job
    Add a dependency mismatcher job that checks whether any dependencies
    have more than one versions.
    
    Signed-off-by: Tomás González <tomasagustin.gonzalezorlando@arm.com>

--- tests/ci.sh
+++ tests/ci.sh
@@ -5,9 +5,43 @@
 
 set -xeuf -o pipefail
 
+error_msg () {
+    echo "Error: $1"
+    exit 1
+}
+
 # Points to Parsec's Unix Domain Socket on the CI
 export PARSEC_SERVICE_ENDPOINT="unix:/tmp/parsec.sock"
 export RUST_LOG=error
+
+##################
+# Get Parameters #
+##################
+MISMATCHER=
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        mismatcher )
+            MISMATCHER="True"
+        ;;
+        *)
+            error_msg "Unknown argument: $1"
+        ;;
+    esac
+    shift
+done
+
+#########################
+# Dependency mismatcher #
+#########################
+if [ "$MISMATCHER" = "True" ]; then
+    python3 $(pwd)/utils/dependency_cross_matcher.py --deps_dir $(pwd)
+    mismatcher_result=$?
+    if [ "$mismatcher_result" -ne 0 ]; then
+        error_msg "Found dependencies version mismatches"
+    fi
+
+    exit 0
+fi
 
 #########
 # Build #
